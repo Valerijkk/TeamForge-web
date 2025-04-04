2b16821c5b826be5ba89f610efa5fa272b5d79de
Valerijkk <154883961+Valerijkk@users.noreply.github.com>
2025-04-05 00:31:27 +0300

    редизайн

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+/* ------------------- pages/MainPage.jsx (Главная страница) ------------------- */
+import React from 'react';
+
+function MainPage() {
+    return (
+        <div className="main-page">
+            <h1>TeamForge</h1>
+            <p>Чат для программистов: общайтесь, делитесь файлами и создавайте команды.</p>
+            <p>Добро пожаловать на нашу главную страницу!</p>
+        </div>
+    );
+}
+
+export default MainPage;
