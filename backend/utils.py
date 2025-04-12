cfdcd196c422ec8edef848d0fe822331542b0b2e
Valerijkk <154883961+Valerijkk@users.noreply.github.com>
2025-04-12 09:24:32 +0300

    Разделение бекенда на модули

---
+++ backend/utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Импортируем URLSafeTimedSerializer для токенов
+from itsdangerous import URLSafeTimedSerializer
+# Импортируем current_app, чтобы использовать Config
+from flask import current_app
+
+# Функция для генерации токена подтверждения/сброса пароля
+def generate_confirmation_token(email):
+    # Создаём сериализатор с секретным ключом приложения
+    ts = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])  # создаём serializer
+    # Возвращаем зашифрованный токен
+    return ts.dumps(email, salt='password-reset-salt')  # dumps() кодирует с солью
+
+# Функция для подтверждения (дешифровки) токена
+def confirm_token(token, expiration=3600):
+    # Пытаемся прочесть токен
+    try:
+        # Снова создаём сериализатор
+        ts = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])  # serializer
+        # Декодируем email
+        email = ts.loads(token, salt="password-reset-salt", max_age=expiration)  # loads() декодирует
+    except Exception:
+        # Если не удалось, возвращаем None
+        return None
+    # Возвращаем email
+    return email
