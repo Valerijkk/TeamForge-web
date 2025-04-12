cf6e70896cd4b997909a4cb22f70678d2eb626e0
Valerijkk <154883961+Valerijkk@users.noreply.github.com>
2025-04-12 09:23:04 +0300

    Разделение бекенда на модули

---
+++ backend/extensions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Импортируем нужные классы
+from flask_sqlalchemy import SQLAlchemy  # Для работы с БД
+from flask_socketio import SocketIO      # Для WebSocket (SocketIO)
+from flask_mail import Mail             # Для отправки почты
+from flask_cors import CORS             # Для CORS
+
+# Создаём объекты расширений
+db = SQLAlchemy()       # Инициализация SQLAlchemy (но без привязки к app)
+socketio = SocketIO()   # Инициализация SocketIO (пока без app)
+mail = Mail()           # Инициализация почтового клиента
+cors = CORS()           # Инициализация CORS
